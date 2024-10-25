c51f4ca775a39bed8e3cf6cdbe6776bec1f63a9e
Abdullah Ahmed <abdullahahmed@Abdullahs-MacBook-Pro.local>
2024-10-25 21:44:41 +0500

    CORS added

--- Backend/app.js
+++ Backend/app.js
@@ -7,9 +7,11 @@ const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // Database connection
 const pool = new Pool({
